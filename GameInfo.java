1be9fc1245ec301932f4cd3eb59bb9e0f19af701
ChloeBarlow <barlow4061@mydusd.org>
2025-08-22 12:56:23 -0700

    Update GameInfo.java

--- GameInfo.java
+++ GameInfo.java
@@ -23,7 +23,7 @@ public class GameInfo extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); 
-        setBackground(new GreenfootImage("background.png"));
+        setBackground(new GreenfootImage("starting_background.jpg"));
         // Initialize the text box.
         textBox = new TextBox();
         // Add the text box to the world.
